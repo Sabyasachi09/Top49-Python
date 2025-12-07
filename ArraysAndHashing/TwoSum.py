cc85620f5f34c5678ab028efb8c172ceecfb20ae
Sabyasachi Choubey <sab.choubey@hotmail.com>
2025-12-07 11:36:32 +0530

    add second solution to two-sum problem

--- ArraysAndHashing/TwoSum.py
+++ ArraysAndHashing/TwoSum.py
@@ -20,7 +20,9 @@ def main():
     target = 9
     print("Array: %r | target: %d" %(array, target))
     solutionOne = twoSumSolutionOne(array, target)
+    solutionTwo = twoSumSolutionTwo(array, target)
     printResult(solutionOne, target, "One")
+    printResult(solutionTwo, target, "Two")
 
 def printResult(result: list[int], target, solution):
     if result:
@@ -36,6 +38,22 @@ def twoSumSolutionOne(array: list[int], target: int):
             if array[lp] + array[rp] == target:
                 return [lp, rp]
     return []
+
+# This solution uses two pointer approach on a sorted array
+# Time Complexity: O(nlog(n)) + O(n) = O(nlog(n))
+def twoSumSolutionTwo(array: list[int], target: int):
+    sortedArray = sorted(array)
+    lp = 0
+    rp = len(sortedArray)-1
+    while(lp < rp):
+        currentSum = array[lp] + array[rp]
+        if currentSum == target:
+            return [lp, rp]
+        elif currentSum > target:
+            rp -= 1
+        else:
+            lp += 1
+    return [] 
 
 if __name__ == "__main__":
     main()
